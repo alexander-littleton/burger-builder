b824933507a80d75e6de6e04cbb308b906fa57f4
littale12 <littale12@gmail.com>
2021-04-01 12:02:21 -0400

    first staging commit

--- src/containers/BurgerBuilder/Burgerbuilder.js
+++ src/containers/BurgerBuilder/Burgerbuilder.js
@@ -1,13 +1,105 @@
 import React, {Component} from "react"
+
 import Aux from "../../hoc/Auxillary"
 import Burger from "../../components/Burger/Burger"
+import BuildControls from "../../components/Burger/BuildControls/BuildControls"
+import Modal from "../../components/UI/Modal/Modal"
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients : {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4,
+        purchaseable: false,
+        purchasing: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchaseable: sum > 0})
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        //use of spread below is important since it does not alter original state
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        /*even though the state is updated with the new ingredients first, 
+        because .setState() updates asynchronously, it does not update before
+        this.updatePurchaseState() is called so we have to pass the updated 
+        ingredients for it to work properly
+        */
+        this.updatePurchaseState(updatedIngredients); 
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        if (oldCount) {
+            const updatedCount = oldCount - 1
+            const updatedIngredients = {
+                ...this.state.ingredients
+            }
+            updatedIngredients[type] = updatedCount
+            const priceDeduction = INGREDIENT_PRICES[type]
+            const oldPrice = this.state.totalPrice;
+            const newPrice = oldPrice - priceDeduction;
+            this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+            this.updatePurchaseState(updatedIngredients);
+        }
+        
+        
+    }
+    
+    purchaseHandler= () => {
+        this.setState({purchasing:true})
+    }
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return(
             <Aux>
-                <Burger/>
-                <div>Build Controls</div>
+                <Modal show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchaseable}
+                    price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
+                />
+                    
             </Aux>
         );
     }
